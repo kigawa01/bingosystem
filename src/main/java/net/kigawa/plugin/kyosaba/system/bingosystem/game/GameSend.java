ac071fa54a5b3f8d1d6535465934e5abee0f5f00
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 01:24:27 +0900

    marge to use gate

--- src/main/java/net/kigawa/plugin/kyosaba/system/bingosystem/game/GameSend.java
+++ src/main/java/net/kigawa/plugin/kyosaba/system/bingosystem/game/GameSend.java
@@ -21,33 +21,11 @@ public class GameSend implements CommandExecutor {
     }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Server server=plugin.getServer();
-
-        Location[] startLoc={
-                new Location(plugin.getServer().getWorld(label),Integer.parseInt(args[0]),Integer.parseInt(args[1]),Integer.parseInt(args[2]) ),
-                new Location(plugin.getServer().getWorld(label),Integer.parseInt(args[3]),Integer.parseInt(args[4]),Integer.parseInt(args[5]) )
-        };
-        World world=startLoc[0].getWorld();
-        assert world != null;
-        List<Player> players=world.getPlayers();
-        Player player=null;
-        Iterator<Player> it=players.iterator();
-        boolean isGet=false;
-        double[] HL={startLoc[0].getX(),startLoc[0].getY(),startLoc[0].getZ()};
-        double[] LL={startLoc[1].getX(),startLoc[1].getY(),startLoc[1].getZ()};
-        while (it.hasNext()&&!isGet){
-            player=(Player) it.next();
-            Location locationPlayer=player.getLocation();
-            double[] PL={locationPlayer.getX(),locationPlayer.getY(),locationPlayer.getZ()};
-            if(LL[0]<=PL[0]&&PL[0]<=HL[0]&&LL[1]<=PL[1]&&PL[1]<=HL[1]&&LL[2]<=PL[2]&&PL[2]<=HL[2]){
-                isGet=true;
-            }
-        }
-        if(isGet){
+        Player player=(Player) sender;
             GameData gameData=plugin.getGameList().getGameData();
             if(!(gameData ==null)){
                 int price=plugin.getConfig().getInt("price");
-                if(plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "money take "+sender.getName()+" "+price)){
+                if(plugin.getServer().dispatchCommand(sender, "money take "+sender.getName()+" "+price)){
                     player.teleport(gameData.startLoc);
                     List<GameBord> gameBords=gameData.gameBords;
                     GameBord gameBord=gameBords.get(gameBords.indexOf(null));
@@ -57,9 +35,6 @@ public class GameSend implements CommandExecutor {
             }else {
                 player.sendMessage( ChatColor.DARK_RED+ "ビンゴの空きがありません");
             }
-        }else {
-            sender.sendMessage(ChatColor.DARK_RED+ "player is exit");
-        }
 
 
         return false;
